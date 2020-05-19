fc56377d6ac828c453f336d7a85771c5d0b5610f
Victor Alcantara <victoralcan@gmail.com>
2020-05-19 19:18:17 -0300

    ESLint fix

--- commands/coronabrasil.js
+++ commands/coronabrasil.js
@@ -35,11 +35,11 @@ exports.run = async (client, message, args) => {
           }\nMortes: ${
             args[0]
               ? args[1] !== undefined
-                ? (data[0].mortes === undefined 
+                ? (data[0].mortes === undefined
                   ? '0'
                   : data[0].mortes)
-                : (data.mortes === undefined 
-                  ? '0' 
+                : (data.mortes === undefined
+                  ? '0'
                   : data.mortes)
               : data.totalMortes
           }`,
